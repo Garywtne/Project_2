9fa59490fc3cadd890fd527f15ba9d16c418112b
grilgamesh <grilly@gmail.com>
2022-05-28 10:07:24 +0100

    uploading of all files

---
+++ config.py
@@ -0,0 +1,4 @@
+gkey = "AIzaSyDh1csNUkvkohSH2nNsZwRasBs0Fa91Zh4"
+censusKey = "80dcce5dd0c7156d9a531715ff44d8cac2a2ef34"
+openWeatherKey = "46a42e01d811a6498480f44172d75bf0"
+postgresPass = "$treaks2021"
